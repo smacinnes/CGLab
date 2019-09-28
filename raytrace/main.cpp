c2c69321dee3a9d6163f1e986e1014daebd5fa3c
smacinnes <40182280+smacinnes@users.noreply.github.com>
2019-09-28 12:51:47 -0300

    improvements
    Optimized code for Vec3 and scalar multiplications. 
    Improved sphere intersection calculations
    Patched coordinate error

--- raytrace/main.cpp
+++ raytrace/main.cpp
@@ -10,26 +10,21 @@ Colour blue()   { return Colour(0.0f, 0.0f, 1.0f); }
 Colour white()  { return Colour(1.0f, 1.0f, 1.0f); }
 Colour black()  { return Colour(0.0f, 0.0f, 0.0f); }
 
-// colours should be expressed 0-1 so modify this or divide answer by 256
+// bind a single pixel value in range [0.0f,1.0f]
 float bindPixel(float value){
     if (value <= 0)    return 0.0f;
     if (value >= 1.0f) return 1.0f;
     return value;
 }
+// bind a all pixel values in range [0.0f,1.0f]
 Vec3 bindPixelValues(const Vec3& v){
     return Vec3(bindPixel(v(0)),bindPixel(v(1)),bindPixel(v(2)));
 }
-// multiply two vectors elementwise - ex for colours
+// multiply two vectors elementwise
 Vec3 mult(const Vec3& v1,const Vec3& v2){
     return Vec3(v1(0)*v2(0),v1(1)*v2(1),v1(2)*v2(2));
 }
-// multiply a vector by a scaler - is there really no function for this already?
-Vec3 mult(const Vec3& v,float s){
-    return Vec3(v(0)*s,v(1)*s,v(2)*s);
-}
-Vec3 mult(float s,const Vec3& v){
-    return Vec3(v(0)*s,v(1)*s,v(2)*s);
-}
+// print a vector for debugging purposes
 void printVec(const Vec3& v){
     printf("%.3f  %.3f  %.3f",double(v(0)),double(v(1)),double(v(2)));
 }
@@ -41,16 +36,19 @@ void printlnVec(const Vec3& v){
  *  The point (0,0,0) will be an absolute position in space and is not defined
  *  relative to the camera, image plane or anything else in the scene. The
  *  distance from the camera to the image plane is defined as 1.0 (but this
- *  can be changed under ImagePlane) and all other dimensions will be modeled
+ *  can be changed under ImagePlane) and all other dimensions will be scaled
  *  off of that distance.
  */
 
+// Class defining the position and orientation of the camera
+// TODO: add easier adjustment of tilting function (ex auto calculate up
+// vector from the tilt in radians)
 class Camera {
 public:
     // set these three manually
     Vec3 position =  Vec3(0,0,5);   // location of body of camera
     Vec3 lookingAt = Vec3(0,0,4);   // where the camera is facing
-    Vec3 up = Vec3(0,1,0);          // tilt of camera (0,1,0) is no tilt - add easier adjustment
+    Vec3 up = Vec3(0,1,0);          // tilt of camera: (0,1,0) is no tilt
     // these will be set during setup() call
     Vec3 viewingDir = Vec3(0,0,0);
     Vec3 rightAxis = Vec3(0,0,0);
@@ -61,10 +59,11 @@ public:
         viewingDir.normalize();
         rightAxis = up.cross(viewingDir);
         rightAxis.normalize();
-        upAxis = viewingDir.cross(rightAxis);
+        upAxis = viewingDir.cross(rightAxis); // will be unit vec
     }
 };
 
+// Class defining all properties of the image plane
 class ImagePlane {
 private:
     // set these manually
@@ -72,12 +71,10 @@ private:
     int hResolution = 480;      // default
     float viewingAngle = 90.0f; // recommended - most natural for humans
     float distToCam = 1.0f;     // all other dimensions based off this
-    // rest are calculated automatically
+    // these are calculated automatically
     Vec3 center = Vec3(0,0,0);
-    Vec3 corner = Vec3(0,0,0);
     float hwRatio = float(hResolution)/wResolution;
     float halfWidth = 0.0f;
-
 public:
     Image<Colour> image = Image<Colour>(hResolution, wResolution);
     Vec3 llc = Vec3(0,0,0);   // lower left corner
@@ -85,11 +82,11 @@ public:
     Vec3 pixUp = Vec3(0,0,0); // move one pixel up
 
     void setup(const Camera& c){
-        center = c.position+mult(c.viewingDir,distToCam);
+        center = c.position+c.viewingDir*distToCam;
         halfWidth = distToCam*tanf(viewingAngle/2.0f);
-        llc = center - mult(c.rightAxis,halfWidth) - mult(c.upAxis,halfWidth*hwRatio);
-        pixRi = mult(2*halfWidth/(wResolution-1),c.rightAxis);
-        pixUp = mult(2*halfWidth*hwRatio/(hResolution-1),c.upAxis);
+        llc = center - c.rightAxis*halfWidth - c.upAxis*halfWidth*hwRatio;
+        pixRi = 2*halfWidth/(wResolution-1)*c.rightAxis;
+        pixUp = 2*halfWidth*hwRatio/(hResolution-1)*c.upAxis;
     }
 };
 
@@ -110,29 +107,41 @@ public:
 private:
     Vec3 co = Vec3(0,0,0);      //center of object to origin of ray
     float disc = 0.0f;
-    float t = 0.0f;
+    float t1 = 0.0f;
+    float t2 = 0.0f;
     float b = 0.0f;
 public:
     // intersect image plane at specified pixel
     void constructPrimary(const ImagePlane& im,int row,int col,const Camera& cam){
-        orig = im.llc + mult(im.pixRi,col-1) + mult(im.pixUp,row);
+        orig = im.llc + im.pixRi*float(col-1) + im.pixUp*float(row-1);
         // pixel direction as unit vector
         dir = orig - cam.position;
         dir.normalize();
     }
-    // if the ray intersects the given sphere
+    // returns true iff the ray has a valid intersection with the given sphere
+    // "valid" meaning that at least one solution is positive
+    // two pos solutions:   ray starts before sphere    (desired)
+    // one pos solution:    ray starts inside sphere    (cross section - optional)
+    // two neg solution:    ray starts after sphere     (ignore)
+    // two comp solutions:  ray does not intersect      (ignore)
+    // this function stores the discriminant value and any solution values
+    // it calculates to avoid unnecessary recalculations
     bool intersects(const Sphere& s){
         co = orig-s.center;
-        disc = powf(dir.dot(co),2)-(powf(co.norm(),2)-powf(s.radius,2));
-        return disc >= 0.0f;
+        disc = powf(dir.dot(co),2)-powf(co.norm(),2)+powf(s.radius,2);
+        if (disc < 0.0f) return false;
+        b = -(dir.dot(co));
+        t1 = b+sqrt(disc);
+        t2 = b-sqrt(disc);
+        return t1 > 0;                  // change to t2 to avoid cross sections
     }
-    // assumes intersects() has already been called
+
+    // assumes intersects() has already been evaluated to true
+    // therefore disc, t1 and t2 have been calculated
     // add error message printouts for invalid disc and t?
-    Vec3 findSphereIntersection(){
-        if (disc < 0) return Vec3(0,0,0);
-        b = -dir.dot(co);
-        t = fmax(fmin(b+sqrt(disc),b-sqrt(disc)),0.0f);
-        return orig + t*dir;
+    Vec3 sphereIntersection(){
+        if (t2>0) return orig+t2*dir;
+        return orig+t1*dir;
     }
 };
 
@@ -168,44 +177,57 @@ public: // properties
         normal.normalize();
         lightAngle = intersection - position;
         lightAngle.normalize();
-        Id = mult(mult(s.kd,float(lightAngle.dot(normal))),diffuse);
+        Id = mult(lightAngle.dot(normal)*s.kd,diffuse);
 
         // specular light calculation
-        R = mult(2*lightAngle.dot(normal),normal)-lightAngle;
-        Is = mult(mult(s.ks,pow(R.dot(-r.dir),s.alpha)),specular);
+        R = 2*lightAngle.dot(normal)*normal-lightAngle;
+        Is = mult(s.ks*pow(R.dot(-r.dir),s.alpha),specular);
 
         bindValues();
         return (Ia + Id + Is);
     }
 };
 
+// sets up the camera and image plane
+// when moving objects in the scene, the x-direction is reversed from what
+// would be intuitive, except when moving the light sources, in which case
+// the x-axis is intuitive but the others are not. Correct that behavior here
+// so the user can define all objects intuitively
+void setup(Camera& cam,ImagePlane& im,Sphere& s,LightSource& L){
+    cam.position    = mult(cam.position, Vec3(-1,1,1));
+    cam.lookingAt   = mult(cam.lookingAt,Vec3(-1,1,1));
+    s.center        = mult(s.center,     Vec3(-1,1,1));
+    L.position      = mult(L.position,   Vec3(1,-1,-1));
 
+    cam.setup();
+    im.setup(cam);
+}
 
 int main(int, char**){
+    // (+x: right +y: up +z: out of screen)
 
-    /* PROBLEMS
-     * when moving objects around scene, x always seems reversed except when moving
-     * light source, in which case x is fine and the other directions seem reversed
-     */
     // DEFINE CAMERA
-    Camera cam = {Vec3(0,0,10),Vec3(0,0,-10)}; // {position,lookingAt}
-    cam.setup();
+    // {position,lookingAt}
+    Camera cam = {Vec3(0,0,4),Vec3(0,0,0)};
 
     // DEFINE IMAGE PLANE
+    // default is 640x480 w/ 90 deg view angle
     ImagePlane im;
-    im.setup(cam);
 
-    // DEFINE OBJECTS IN SCENE (+x: right +y: up +z: close)
-    Sphere s = {Vec3(0,0,2),1,Colour(.6f,.2f,.6f),Colour(.6f,.2f,.6f),Colour(.6f,.2f,.6f),5};
+    // DEFINE OBJECTS IN SCENE
+    // {position,radius,ambient,diffuse,specular,alpha}
+    Sphere s = {Vec3(0,0,0),1,Colour(.6f,.2f,.6f),Colour(.6f,.2f,.6f),Colour(.6f,.2f,.6f),5};
 
     // DEFINE LIGHTING SOURCES
-    LightSource L = {Vec3(0,0,-5),Colour(.6f,.6f,.6f),Colour(.6f,.6f,.6f),Colour(.6f,.6f,.6f)};
+    // {position,ambient,diffuse,specular}
+    LightSource L = {Vec3(5,5,2),Colour(.6f,.6f,.6f),Colour(.6f,.6f,.6f),Colour(.6f,.6f,.6f)};
+
+    setup(cam,im,s,L);
 
     // INITIALIZE LOOP VARIABLES
     Ray pixel;
-    Vec3 intersection = Vec3(0,0,0);
 
-    // test for image plane intersecting any objects?
+    // add test for image plane intersecting any objects?
 
 
     for (int row = 0; row < im.image.rows(); ++row) {
@@ -214,9 +236,8 @@ int main(int, char**){
             pixel.constructPrimary(im,row,col,cam);
 
             if (pixel.intersects(s)) {
-                intersection = pixel.findSphereIntersection();
 
-                im.image(row,col) = L.illuminate(s,pixel,intersection);
+                im.image(row,col) = L.illuminate(s,pixel,pixel.sphereIntersection());
 
             } else {
                 im.image(row,col) = black();
