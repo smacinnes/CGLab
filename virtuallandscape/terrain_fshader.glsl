a052baf65e67edd1bf3c3ae738d0f380bf6c8398
smacinnes <40182280+smacinnes@users.noreply.github.com>
2019-11-30 17:36:41 -0400

    added colouring and texturing

--- virtuallandscape/terrain_fshader.glsl
+++ virtuallandscape/terrain_fshader.glsl
@@ -24,22 +24,52 @@ void main() {
 
     // Texture size in pixels
     ivec2 size = textureSize(noiseTex, 0);
-
     /// TODO: Calculate surface normal N
     /// HINT: Use textureOffset(,,) to read height at uv + pixelwise offset
     /// HINT: Account for texture x,y dimensions in world space coordinates (default f_width=f_height=5)
-    vec3 A = vec3( 0 );
-    vec3 B = vec3( 0 );
-    vec3 C = vec3( 0 );
-    vec3 D = vec3( 0 );
-    vec3 N = normalize( cross(normalize(A-B), normalize(C-D)) );
 
+    // temp colouring before textures are applied
+    vec3 N  = vec3(0,0,1);
+    if (texture2D(noiseTex,uv).r > 0.0f){
+        vec3 A = vec3(uv.x +1.0/size.x, uv.y, textureOffset(noiseTex, uv, ivec2(1,0)));
+        vec3 B = vec3(uv.x -1.0/size.x, uv.y, textureOffset(noiseTex, uv, ivec2(-1,0)));
+        vec3 C = vec3(uv.x, uv.y +1.0/size.y, textureOffset(noiseTex, uv, ivec2(0,1)));
+        vec3 D = vec3(uv.x, uv.y -1.0/size.y, textureOffset(noiseTex, uv, ivec2(0,-1)));
+        N = normalize( cross(normalize(A-B), normalize(C-D)) );
+    }
     /// TODO: Texture according to height and slope
     /// HINT: Read noiseTex for height at uv
+    // options for texture are water,sand,grass,rock,snow
+    float waterLevel = 0.0f;    // all below this is water
+    float sandLevel = 0.02f;    // sand can only be below this
+    float sandThresh = 0.1f;    // sand only if not this steep
+    float snowLevel = 0.5f;     // snow can only be above this
+    float rockThresh = 0.8f;    // rock if steeper than this
+
+    float height = texture2D(noiseTex,uv).r;
+    float incline = abs(dot(vec3(0,0,1),N));
+
+    // some tweaking to do - ex repeating textures
+    if (height <= waterLevel) {
+        color = texture(water,uv);
+    } else if (incline >= rockThresh) {
+        color = texture(rock,uv);
+    } else if (height >= snowLevel) {
+        color = texture(snow,uv);
+    } else if (height <= sandLevel && incline <= sandThresh) {
+        color = texture(sand,uv);
+    } else {
+        color = texture(grass,uv);
+    }
 
     /// TODO: Calculate ambient, diffuse, and specular lighting
     /// HINT: max(,) dot(,) reflect(,) normalize()
+    // make dependant on texture?
+    float ambient = 0.5f;
+    float diffuse;
 
-    color = vec4(0,0,0,1);
+    //color = vec4(0,0,0,1);
+
+    //color = vec4((N + vec3(1.0)) / 2.0f,1);
 }
 )"
